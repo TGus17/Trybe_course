68b4efd0a5c107c733f69fe53a6f6fdc2660e771
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-13 20:19:38 -0300

    Exercicio 9: True caso seja ímpar

--- Exercicios/Bloco4_JavaScript/Exercicios4_1.js
+++ Exercicios/Bloco4_JavaScript/Exercicios4_1.js
@@ -1,10 +1,10 @@
-let num1 = 3;
-let num2 = 21;
-let num3 = 35;
+let num1 = 6;
+let num2 = 20;
+let num3 = 30;
 
 isOdd = false;
 
-if ((num1 % 2 == 0) || (num2 % 2 == 0) || (num3 % 2 == 0)){
+if ((num1 % 2 != 0) || (num2 % 2 != 0) || (num3 % 2 != 0)){
   isOdd = true;
 } else {
   isOdd = false;
