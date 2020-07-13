19e1dc84bdc7e11a55745f1e2cee1ea0f3e9a979
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-13 18:39:53 -0300

    Exercicio6: Sem o bônus ainda

--- Exercicios/Bloco4_JavaScript/Exercicios4_1.js
+++ Exercicios/Bloco4_JavaScript/Exercicios4_1.js
@@ -1,5 +1,24 @@
-let lado1 = 180;
-let lado2 = 50;
-let lado3 = 0;
+let peca = "casa";
 
-(lado1 + lado2 + lado3) == 180 ? console.log("true") : console.log("Erro! Lados não correspondem a um triângulo!");
+switch (peca) {
+  case "cavalo":
+    console.log(peca + " -> Movimento em L");
+    break;
+  case "peao":
+    console.log(peca + " -> Ande uma casa a frente");
+		break;
+	case "bispo":
+		console.log(peca + " -> Ande na diagonal");
+		break;
+	case "rainha":
+		console.log(peca + " -> Anda em qualer direção");
+		break;
+	case "torre":
+		console.log(peca + " -> Anda em movimentos perpendiculares");
+		break;
+	case "rei":
+		console.log(peca + " -> Anda somente uma casa em torno de si");
+		break;
+	default:
+		console.log(peca + " -> Não é uma peça válida");    
+}
