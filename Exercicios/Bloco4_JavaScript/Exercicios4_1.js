946bb7281d5ad99b68434c84e6207c302352714d
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-13 18:10:24 -0300

    Exercicio3: Maior valor entre três números

--- Exercicios/Bloco4_JavaScript/Exercicios4_1.js
+++ Exercicios/Bloco4_JavaScript/Exercicios4_1.js
@@ -1,8 +1,15 @@
-let num1 = 4;
-let num2 = 6;
+let num1 = 10;
+let num2 = 12;
+let num3 = 9;
 
-if (num1 > num2){
-    console.log(num1);
+if (num1 > num2) {
+    maior = num1;
 } else {
-    console.log(num2);
+    maior = num2;
+}
+
+if (maior > num3) {
+    console.log(maior);
+} else {
+    console.log(num3);
 }
