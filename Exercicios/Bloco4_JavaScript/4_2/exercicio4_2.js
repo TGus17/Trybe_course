4db46d46482d678f82474d34389f631b1643a839
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-14 17:57:39 -0300

    Exercício_2: Soma dos valores

--- Exercicios/Bloco4_JavaScript/4_2/exercicio4_2.js
+++ Exercicios/Bloco4_JavaScript/4_2/exercicio4_2.js
@@ -1,5 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
 
 for (let indice = 0; indice < numbers.length; indice += 1){
-    console.log(numbers[indice]);
+    sum = sum + numbers[indice];
 }
+
+console.log("A soma dos valores foi de: " + sum);
