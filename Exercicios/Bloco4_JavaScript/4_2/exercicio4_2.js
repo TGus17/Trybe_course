0f210468d5984642283a6bf6708713e1731731a0
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-14 18:11:06 -0300

    Exercício_7: Menor valor no array

--- Exercicios/Bloco4_JavaScript/4_2/exercicio4_2.js
+++ Exercicios/Bloco4_JavaScript/4_2/exercicio4_2.js
@@ -1,14 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let impar = 0;
+let menor = 100;
 
 for (let indice = 0; indice < numbers.length; indice += 1){
-    if ((numbers[indice] % 2) != 0) {
-        impar += 1;
+    if (numbers[indice] < menor) {
+        menor = numbers[indice];
     }
 }
 
-if (impar === 0) {
-    console.log("Nenhum valor ímpar encontrado.");
-} else {
-    console.log("Quantidade de números ímpares no array: " + impar);
-}
+console.log("O menor valor do array é: " + menor);
