0e0b1689822223c555a1210a16f751d1b65d4545
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-14 17:59:25 -0300

    Exercício_3: Média aritimética dos valores

--- Exercicios/Bloco4_JavaScript/4_2/exercicio4_2.js
+++ Exercicios/Bloco4_JavaScript/4_2/exercicio4_2.js
@@ -1,8 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+let media = 0;
 
 for (let indice = 0; indice < numbers.length; indice += 1){
     sum = sum + numbers[indice];
 }
 
-console.log("A soma dos valores foi de: " + sum);
+media = sum / (numbers.length);
+console.log("A média aritimética dos valores foi de: " + media);
