6110f9fca4f706ce1c62ce821a12cb1e844f5a04
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-14 18:08:43 -0300

    Exercício_6: Quantidade de números ímpares

--- Exercicios/Bloco4_JavaScript/4_2/exercicio4_2.js
+++ Exercicios/Bloco4_JavaScript/4_2/exercicio4_2.js
@@ -1,10 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior = 0;
+let impar = 0;
 
 for (let indice = 0; indice < numbers.length; indice += 1){
-    if (numbers[indice] > maior) {
-        maior = numbers[indice];
+    if ((numbers[indice] % 2) != 0) {
+        impar += 1;
     }
 }
 
-console.log("Maior valor contido no array é: " + maior);
+if (impar === 0) {
+    console.log("Nenhum valor ímpar encontrado.");
+} else {
+    console.log("Quantidade de números ímpares no array: " + impar);
+}
