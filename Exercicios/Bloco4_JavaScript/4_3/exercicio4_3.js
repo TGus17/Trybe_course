cf2fff44e63b8e47b838441539ac736384d0bb69
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-15 18:20:04 -0300

    Exercício 2: Triângulo de asteríscos

--- Exercicios/Bloco4_JavaScript/4_3/exercicio4_3.js
+++ Exercicios/Bloco4_JavaScript/4_3/exercicio4_3.js
@@ -1,15 +1,8 @@
-let n = 9;
+let n = 7;
+let triângulo = "*";
 
-
-if (n === 0) {
-	console.log("Valor indefinido")
-} else {
-	for (indice = 0; indice < n; indice += 1){
-		let quadrado = "*";
-		for (let index = 1; index < n; index += 1) {
-		quadrado += "*";
-		}
-		console.log(quadrado);
-	}
+for (let index = 1; index <= n; index += 1) {
+	console.log(triângulo);
+	triângulo += "*";
 }
 
