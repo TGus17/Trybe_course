5491d35aabc63803154db97c530471fa8bc06ac2
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-16 15:50:37 -0300

    Exercício 2: Imprimindo ifo

--- Exercicios/Bloco4_JavaScript/4_4/exercicio4_4.js
+++ Exercicios/Bloco4_JavaScript/4_4/exercicio4_4.js
@@ -2,8 +2,9 @@ let info = {
     personagem: "Margarida",
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim"
   };
 
-  //1. Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+  //2. Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.
 
-  console.log("Bem-vinda, " + info.personagem);
+  console.log(info);
