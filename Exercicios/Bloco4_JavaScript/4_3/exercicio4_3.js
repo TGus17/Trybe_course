1cc94385ffb8f3c9e41b85a9f080cdd0e1ad1426
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-17 13:22:05 -0300

    Bonus_Exercício 5: Pirâmide vazada

--- Exercicios/Bloco4_JavaScript/4_3/exercicio4_3.js
+++ Exercicios/Bloco4_JavaScript/4_3/exercicio4_3.js
@@ -1,4 +1,4 @@
-//4. Depois, faça uma pirâmide com n asteriscos de base:
+//5. Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
 
 let n = 11;
 let linha;
