2cabc8ee3ca5af301ee7c1746417c0e3dbaee5f6
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-24 10:13:38 -0300

    ParteII_6: Somatorio até N

--- Exercicios/Bloco4_JavaScript/4_4/partII_exercicio4_4.js
+++ Exercicios/Bloco4_JavaScript/4_4/partII_exercicio4_4.js
@@ -1,23 +1,12 @@
-//5. Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+//6. Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
 
 
-function verificaInteiro(teste) {
-	let repetido = 1;
-	let inteiro = 0;
-	let maior = 0;
-	for (let i = 0; i < teste.length; i += 1) {
-		for (let j = 0; j < teste.length; j += 1) {
-			if (teste[i] === teste[j]) {
-				repetido += 1;
-			}
-		}
-		if (repetido > maior) {
-			maior = repetido;
-			inteiro = teste[i];
-			repetido = 1;
-		} else {
-			repetido = 1;
-		}
+function somatorio(num) {
+	let soma = 0;
+	for (let i = 0; i <= num; i += 1) {
+		soma += i;
 	}
-	return inteiro;
+	return soma;
 }
+
+console.log(somatorio(5));
