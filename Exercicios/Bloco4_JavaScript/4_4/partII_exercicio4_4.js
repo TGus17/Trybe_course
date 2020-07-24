a209302ce47220f389e709ffb3999bae43dbd36b
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-24 10:10:11 -0300

    ParteII_5: Maior qtdade repetição

--- Exercicios/Bloco4_JavaScript/4_4/partII_exercicio4_4.js
+++ Exercicios/Bloco4_JavaScript/4_4/partII_exercicio4_4.js
@@ -1,17 +1,23 @@
-//4. Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+//5. Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 
 
-function verificaCaracteres (teste) {
-	let palavra = '';
-	let menor = 0;
-	
-	
-	for (let index in teste){		
-		if ((teste[index].length) > menor) {
-			menor = teste[index].length;
-			palavra = teste[index];
-		} 
-	}	
-	return palavra;
+function verificaInteiro(teste) {
+	let repetido = 1;
+	let inteiro = 0;
+	let maior = 0;
+	for (let i = 0; i < teste.length; i += 1) {
+		for (let j = 0; j < teste.length; j += 1) {
+			if (teste[i] === teste[j]) {
+				repetido += 1;
+			}
+		}
+		if (repetido > maior) {
+			maior = repetido;
+			inteiro = teste[i];
+			repetido = 1;
+		} else {
+			repetido = 1;
+		}
+	}
+	return inteiro;
 }
-
