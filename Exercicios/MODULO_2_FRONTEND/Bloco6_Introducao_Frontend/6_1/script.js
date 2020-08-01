6dd173ff211a45a36fbf50f577b83115ea20f3de
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-01 15:25:39 -0300

    Exercicio6_1-3,4 Faltando div consolidada

--- Exercicios/MODULO_2_FRONTEND/Bloco6_Introducao_Frontend/6_1/script.js
+++ Exercicios/MODULO_2_FRONTEND/Bloco6_Introducao_Frontend/6_1/script.js
@@ -1,11 +1,19 @@
 window.onload = function () {
   const state = document.querySelector('#state');
-  const states = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];  
+  const states = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
   let value = 1;
   let arr = [];
   let pos = '';
   const date = document.querySelector('#date');
   const form = document.querySelector('#form-curriculo');
+  const cut = document.querySelector('#interromper');
+  const data = document.querySelector('#show');
+  const name = document.querySelector('#name');
+  const email = document.querySelector('#email');
+  const cpf = document.querySelector('#cpf');
+  const option = document.querySelector('option');
+  const mostrar = document.querySelector('#show-datas');
+
 
   function createListStates() {
     for (let index = 0; index < states.length; index += 1) {
@@ -35,18 +43,24 @@ window.onload = function () {
     number = date.value;
     spliDate(number);
     if ((parseInt(arr[0]) < 0) || (parseInt(arr[0]) > 31)) {
-      // number.preventDefault();
       alert('Dia inválido');
     }
     if ((parseInt(arr[1]) < 0) || (parseInt(arr[1]) > 12)) {
-      // number.preventDefault();
       alert('Mes inválido');
     }
     if ((parseInt(arr[2]) < 0)) {
-      // number.preventDefault();
       alert('Ano inválido');
     }
   }
 
-form.addEventListener('submit', verificaDate);
+  function showData() {
+    mostrar.innerHTML = 'Nome: ' + name.value + '\r\n' + 'Email: ' + email.value; 
+  }
+
+  form.addEventListener('submit', function () {
+    verificaDate(event);
+    showData();
+    event.preventDefault();
+  }
+  );
 }
