56595901aa67ecdd25a77bf758aebcf1aedb2d48
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-11 10:58:23 -0300

    4. Função 2

--- Exercicios/MODULO_2_FRONTEND/7_1/7_1_PartII.js
+++ Exercicios/MODULO_2_FRONTEND/7_1/7_1_PartII.js
@@ -1,18 +1,32 @@
+const skills = ['HTML', 'Javascript', 'CSS', 'Bash', 'Shellscript'];
+
 let func = (str) => {
   sentence = 'Gustavo x demais';
   sentence = sentence.split(' ');
   let phrase = '';
-
   for (let index = 0; index < sentence.length; index += 1) {
     if (sentence[index] === 'x') {
       sentence[index] = str;
     }
   }
-
   for (let index = 0; index < sentence.length; index += 1) {
     phrase += `${sentence[index]} `;
   }
-
   return phrase;
 }
 
+
+let func2 = (func) => {
+  let skills2 = skills.sort();
+  let resp = `
+${func}
+
+Minhas cinco habilidades são:`
+  console.log(resp);
+  for (let i = 0; i < skills2.length; i += 1) {
+    console.log(skills2[i]);
+  }
+}
+
+
+func2(func('gostosão'));
