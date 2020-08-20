1c6a830ca4e72f884704f1b34a28ed2b956439a3
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-20 09:53:00 -0300

    Dia 8_3 Exerc. 6 - ainda não finalizado

---
+++ Exercicios/MODULO_2_FRONTEND/8_3/8_3_6.js
@@ -0,0 +1,31 @@
+// 6. Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada objeto terá o formato { name: nome do aluno, average: media das notas }. Para isso vamos assumir que a posição 0 de notas refere-se ao aluno na posição 0 de alunos, aqui além de reduce será necessário utilizar também a função map. Dica: Você pode acessar o index do array dentro de map, e você pode ver o objeto esperado na constante expected.
+
+
+const assert = require('assert');
+const { createSecureContext } = require('tls');
+
+const alunos = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+// const notas = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const notas = [20, 25, 30, 35];
+
+
+function studentAverage() {
+  const array = [];
+  
+  alunos.map((item) => {
+    return notas.reduce((acc, current) => {
+      array.push({ name: item, average: current });
+    }, 0)
+  })
+  return array;
+  }
+
+
+console.log(studentAverage());
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+// assert.deepEqual(studentAverage(), expected);
