31a5669a0a2fb0a6825b2e873af68051e52f9272
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-21 11:22:58 -0300

    Dia 8.4_8

---
+++ Exercicios/MODULO_2_FRONTEND/8_4/8_4_8.js
@@ -0,0 +1,9 @@
+// 8. Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+
+const assert = require('assert')
+
+const greet = (name, salut = 'Hi') => `${salut} ${name}`;
+
+assert.equal(greet("John"), "Hi John")
+assert.equal(greet("John", "Good morning"), "Good morning John")
+assert.equal(greet("Isabela", "Oi"), "Oi Isabela")
