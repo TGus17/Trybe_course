464f4ab5d161e50a25ee5a69088c8afacc716f5b
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-26 19:07:40 -0300

    Exercício 4

--- Exercicios/MODULO_2_FRONTEND/9_1/9_1_4.js
+++ Exercicios/MODULO_2_FRONTEND/9_1/9_1_4.js
@@ -14,7 +14,12 @@ const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now 
 
 const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
 
-// definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback1, callback2) => {
+  const probabilidy = (Math.random() * 100) > 60;
+  const temperature = getMarsTemperature();
+
+  probabilidy ? callback1(temperature) : callback2('Robot is busy');
+}
 
 
 // imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
