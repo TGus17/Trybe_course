40066d470afdd08c6eba40896e9adcca8479182d
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-26 18:46:24 -0300

    Exercício 3 - Refatorado

--- Exercicios/MODULO_2_FRONTEND/9_1/9_1_3.js
+++ Exercicios/MODULO_2_FRONTEND/9_1/9_1_3.js
@@ -11,7 +11,10 @@ const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
 const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
-const sendMarsTemperature = (callback) => callback(8.336);
+const sendMarsTemperature = (callback) => {
+  const temperature = getMarsTemperature();
+  callback(temperature);
+}
 
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
