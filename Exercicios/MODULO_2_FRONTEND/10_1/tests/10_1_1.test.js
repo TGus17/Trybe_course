62f54ed787f7ac4c50ba0e68711bddc0d424d37d
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-09-02 20:07:10 -0300

    Exercício 1

---
+++ Exercicios/MODULO_2_FRONTEND/10_1/tests/10_1_1.test.js
@@ -0,0 +1,23 @@
+const sum = require('../exercicios/10_1_1');
+
+describe('A função sum', () => {
+  it('Deve retornar 9, quando passado 4 e 5', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  it('Deve retornar 0, quando passado 0 e 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  it('Lança um erro quando passado 4 e "5"', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
+
+  it('Lança o erro "parameters must be numbers" quando passado 4 e "5"', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow("parameters must be numbers");
+  });
+})
