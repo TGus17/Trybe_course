8c54e7a85d2022fe683051f1faabfa5469096d4b
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-09-05 12:37:21 -0300

    Ajeitando nomes de arquivos

--- Exercicios/MODULO_2_FRONTEND/7_3/example.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-
-const names1 = ['João', 'Maria', 'Gustavo', 'Samara'];
-const names2 = ['João', 'Marias', 'Gustavo', 'Samara'];
-
-assert.notDeepEqual(names1, names2, 'Theese two are the same');
