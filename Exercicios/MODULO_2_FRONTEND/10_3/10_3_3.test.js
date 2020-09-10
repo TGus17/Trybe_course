9da0544dd506a9b76a7aec62863d52c463abaa0a
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-09-09 23:05:44 -0300

    Alguns ajustes

---
+++ Exercicios/MODULO_2_FRONTEND/10_3/10_3_3.test.js
@@ -0,0 +1,7 @@
+const randomNumber = require('./10_3_1.test');
+
+// 3. Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
+
+test('implementando e resetando', () => {
+
+})
