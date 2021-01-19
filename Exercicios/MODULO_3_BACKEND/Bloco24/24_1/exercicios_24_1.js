7dca55af24a6ac3cc27750800a05fe15972a439d
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-19 18:31:26 -0300

    :construction: Exercicio 11

--- Exercicios/MODULO_3_BACKEND/Bloco24/24_1/exercicios_24_1.js
+++ Exercicios/MODULO_3_BACKEND/Bloco24/24_1/exercicios_24_1.js
@@ -122,3 +122,10 @@ db.movies.updateMany(
     }
   }
 );
+
+// Exercício 11 : Utilizando uma única operação, crie um campo chamado sequels e atribua a ele o valor 0 em todos os documentos.
+
+db.movies.updateMany({},
+  { $set: {
+    sequels: 0,
+  }});
