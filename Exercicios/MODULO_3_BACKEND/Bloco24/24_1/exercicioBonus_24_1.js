743b9dc89d29f3bd44d2098fc0c69f0e34906130
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-19 18:53:43 -0300

    :construction: Exercicio 16

--- Exercicios/MODULO_3_BACKEND/Bloco24/24_1/exercicioBonus_24_1.js
+++ Exercicios/MODULO_3_BACKEND/Bloco24/24_1/exercicioBonus_24_1.js
@@ -23,3 +23,16 @@ db.xmen.updateMany(
     }
   }
 );
+
+// Exercício 16 : Produza uma query onde os mutantes class ômega ou gama passam a ter seu poder de 500 somente se seu poder for menor que 500 .
+
+db.xmen.updateMany(
+  {
+    class: { $in: ["gama", "omega"] },
+  },
+  {
+    $max: {
+      power: 500,
+    }
+  }
+);
