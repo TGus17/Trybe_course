c5e5eb6df5fd31a157b95c93e6ebc5887dc74296
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-19 19:17:24 -0300

    :construction: Exercicio 20

--- Exercicios/MODULO_3_BACKEND/Bloco24/24_1/exercicioBonus_24_1.js
+++ Exercicios/MODULO_3_BACKEND/Bloco24/24_1/exercicioBonus_24_1.js
@@ -90,3 +90,20 @@ db.xmen.updateMany(
     }
   }
 );
+
+// Exercício 20 : Em apenas uma query, adicione o campo áreas com [ "outside" ] a todos os Junior Staff que não tenham o campo area definido.
+
+db.xmen.updateMany(
+  {
+    $and:
+      [
+        { occupation: "Junior Staff" },
+        { area: { $exists: false } },
+      ]
+  },
+  {
+    $set: {
+      áreas: [ "outside" ],
+    }
+  }
+);
