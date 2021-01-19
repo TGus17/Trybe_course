820d7f91eb90b0b337b614687bba3cd3bc69b3a4
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-19 18:20:44 -0300

    :construction: Exercicio 6

--- Exercicios/MODULO_3_BACKEND/Bloco24/24_1/exercicios_24_1.js
+++ Exercicios/MODULO_3_BACKEND/Bloco24/24_1/exercicios_24_1.js
@@ -54,3 +54,16 @@ db.movies.update(
     }
   }
 );
+
+// Exercício 6 : Multiplique por 4 o imdbRating do filme Batman .
+
+db.movies.update(
+  {
+    title: "Batman",
+  },
+  {
+    $mul: {
+      imdbRating: 4,
+    }
+  }
+);
