dfe1b2a313d81a74793a77c9b2fa78fcf8d9f4ab
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-19 18:58:37 -0300

    :construction: Exercicio 18

--- Exercicios/MODULO_3_BACKEND/Bloco24/24_1/exercicioBonus_24_1.js
+++ Exercicios/MODULO_3_BACKEND/Bloco24/24_1/exercicioBonus_24_1.js
@@ -49,3 +49,16 @@ db.xmen.updateMany(
     }
   }
 );
+
+// Exercício 18 : Decremente em 100 o poder dos mutantes não contêm a propriedade class .
+
+db.xmen.updateMany(
+  {
+    class: { $exists: false },
+  },
+  {
+    $inc: {
+      power: -100,
+    }
+  }
+);
