2527d2594a4170436a9a8ea1bfe792b0f009dc99
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-21 20:29:26 -0300

    :construction: Exercicio 16

--- Exercicios/MODULO_3_BACKEND/24_3/exercicios_24_3.js
+++ Exercicios/MODULO_3_BACKEND/24_3/exercicios_24_3.js
@@ -136,3 +136,16 @@ db.movies.updateMany(
     }
   }
 );
+
+// 16. Adicione o campo description no filme Godzilla com o seguinte valor: "The world is beset by the appearance of monstrous creatures, but one of them may be the only one who can save humanity." .
+
+db.movies.updateMany(
+  {
+    title: "Godzilla",
+  }, 
+  {
+    $set: {
+      description: "The world is beset by the appearance of monstrous creatures, but one of them may be the only one who can save humanity."
+    }
+  }
+);
