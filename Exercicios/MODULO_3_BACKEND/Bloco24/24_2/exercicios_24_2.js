f5c39c5f483e3efcce7b8e917fa2716d9d044f69
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-21 15:52:34 -0300

    :construction: Exercicio 12

--- Exercicios/MODULO_3_BACKEND/Bloco24/24_2/exercicios_24_2.js
+++ Exercicios/MODULO_3_BACKEND/Bloco24/24_2/exercicios_24_2.js
@@ -208,3 +208,16 @@ db.movies.updateMany(
     arrayFilters: [{ "item.character": "Coringa" }],
   }
 );
+
+// Exercício 12: Adicione aos atores de cast do character Batman do filme Batman os valores "Michael Keaton" , "Val Kilmer" e "George Clooney" , e deixe o array em ordem alfabética.
+
+db.movies.updateMany(
+  { title: "Batman" },
+  {
+    $push: {
+      "cast.$[elem].actor": { $each: ["Michael Keaton", "Val Kilmer", "George Clooney"],
+    $sort: 1 }
+    }
+  },
+  { arrayFilters: [{ "elem.character": "Batman" }]}
+);
