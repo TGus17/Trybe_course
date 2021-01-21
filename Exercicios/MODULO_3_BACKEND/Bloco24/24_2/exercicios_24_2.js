e6c0134159c0f2eb393acd3497b8ddd44937610a
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-21 15:14:02 -0300

    :construction: Exercicio 7

--- Exercicios/MODULO_3_BACKEND/Bloco24/24_2/exercicios_24_2.js
+++ Exercicios/MODULO_3_BACKEND/Bloco24/24_2/exercicios_24_2.js
@@ -76,3 +76,16 @@ db.movies.updateOne(
       }
     }
   );
+
+  // Exercício 7: Adicione a categoria "90's" aos filmes Batman e Home Alone .
+
+  db.movies.updateMany(
+    {
+      $or: [{title: 'Batman'}, {title: 'Home Alone'}],
+    },
+    {
+      $push: {
+        category: "90's",
+      }
+    }
+  );
