aaf751b8799ce66ae736b797bc55b2f2cafb9f0d
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-21 15:04:14 -0300

    :construction: Exercicio 5

--- Exercicios/MODULO_3_BACKEND/Bloco24/24_2/exercicios_24_2.js
+++ Exercicios/MODULO_3_BACKEND/Bloco24/24_2/exercicios_24_2.js
@@ -50,3 +50,16 @@ db.movies.updateOne(
       }
     }
   );
+
+  // Exercício 5: Remova o último elemento do array category do filme Batman .
+
+  db.movies.updateOne(
+    {
+      title: "Batman",
+    },
+    {
+      $pop: {
+        category: 1,
+      }
+    }
+  );
