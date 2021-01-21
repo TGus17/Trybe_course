4dd67f9f9ac961b8bb419720b9c7293ea302a3fd
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-21 20:26:06 -0300

    :construction: Exercicio 14

--- Exercicios/MODULO_3_BACKEND/24_3/exercicios_24_3.js
+++ Exercicios/MODULO_3_BACKEND/24_3/exercicios_24_3.js
@@ -112,3 +112,14 @@ db.movies.find(
     title: 1, ratings: 1, category: 1,
   }
 );
+
+// 14. Retorne os filmes em que o ratings possua tamanho 4 e que seja da category "adventure" ou "family" , mas que não tenha o imdbRating menor que 7.
+
+db.movies.find(
+  { $and: [
+    { ratings: { $size: 4 } },
+    { category: { $in: ['adventure', 'family'] } },
+    { imdbRating: { $gte: 7 } },
+    ]
+  }
+);
