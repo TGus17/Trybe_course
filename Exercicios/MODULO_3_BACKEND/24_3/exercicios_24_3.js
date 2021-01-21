17c803e2905aa3a5d6314988a9dd1fc12a899ab3
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-21 16:13:16 -0300

    :construction: Exercicio 3

--- Exercicios/MODULO_3_BACKEND/24_3/exercicios_24_3.js
+++ Exercicios/MODULO_3_BACKEND/24_3/exercicios_24_3.js
@@ -11,3 +11,13 @@ db.movies.find(
     imdbRating: { $gt: 7 },
   }
 );
+
+// 3. Adicione um array chamado ratings ao filme Batman com os seguintes valores: [85, 100, 102, 105] . Dica: lembre-se do operador $each visto no dia anterior.
+
+db.movies.updateMany(
+  { title: "Batman" },
+  { $push: {
+    ratings: { $each: [85, 100, 102, 105]}
+  }},
+  { upsert: true },
+);
