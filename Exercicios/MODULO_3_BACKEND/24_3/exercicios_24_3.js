1f50bb5294902fa42e90b30a71e8e67fd9b62b9b
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-21 16:19:42 -0300

    :construction: Exercicio 6

--- Exercicios/MODULO_3_BACKEND/24_3/exercicios_24_3.js
+++ Exercicios/MODULO_3_BACKEND/24_3/exercicios_24_3.js
@@ -41,3 +41,10 @@ db.movies.updateMany(
   }},
   { upsert: true },
 );
+
+// 6. Retorne todos os filmes com ratings maior do que 103 , exibindo apenas os campos title e ratings .
+
+db.movies.find(
+  { ratings: { $elemMatch: { $gt: 103 } } },
+  { title: 1, ratings: 1 },
+);
