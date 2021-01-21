26b6a8ec2b6215a11a48ca4794681b64f0647c3c
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-21 13:55:03 -0300

    :construction: Exercicio 2

--- Exercicios/MODULO_3_BACKEND/Bloco24/24_2/exercicios_24_2.js
+++ Exercicios/MODULO_3_BACKEND/Bloco24/24_2/exercicios_24_2.js
@@ -9,3 +9,18 @@ db.movies.updateOne(
       category: 'superhero',
     }
   });
+
+  // Exercício 2: Utilizando o modificador $each , adicione as categorias "villain" e "comic-based" ao filme Batman .
+
+  db.movies.updateOne(
+    {
+      title: 'Batman',
+    },
+    {
+      $push: {
+        category: { $each:
+          ['villain', 'comic-based']
+        }
+      }
+    }
+  );
