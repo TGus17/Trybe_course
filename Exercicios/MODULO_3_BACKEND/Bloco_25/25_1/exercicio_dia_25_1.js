f58147708ef8d04c7ef49b5339f287633dc4fc14
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-25 18:43:49 -0300

    :construction: Ex. 3

--- Exercicios/MODULO_3_BACKEND/Bloco_25/25_1/exercicio_dia_25_1.js
+++ Exercicios/MODULO_3_BACKEND/Bloco_25/25_1/exercicio_dia_25_1.js
@@ -13,3 +13,17 @@ db.clientes.aggregate([
       { dataNascimento: { $gte: ISODate("1995-01-01T00:00:00.0Z"), $lt: ISODate("2006-01-01T00:00:00.000Z")}}
     ]
   } }]);
+
+// Exercício 3: Utilizando o estágio $match , escreva uma agregação para retornar somente os clientes do sexo "FEMININO" e com data de nascimento entre os anos de 1995 e 2005 , limitando a quantidade de documentos retornados em 5 .
+
+db.clientes.aggregate([
+  { $match: { 
+    $and: [
+      { sexo: "FEMININO" },
+      { dataNascimento: { $gte: ISODate("1995-01-01T00:00:00.0Z"), $lt: ISODate("2006-01-01T00:00:00.000Z")}}
+    ]},
+  },
+  {
+    $limit: 5,
+  }
+]);
