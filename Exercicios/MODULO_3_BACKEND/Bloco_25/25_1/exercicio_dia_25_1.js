ec10324e0cc65dd5c48c29fb77a727d84494d30b
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-25 18:41:06 -0300

    :construction: Ex. 2

--- Exercicios/MODULO_3_BACKEND/Bloco_25/25_1/exercicio_dia_25_1.js
+++ Exercicios/MODULO_3_BACKEND/Bloco_25/25_1/exercicio_dia_25_1.js
@@ -3,3 +3,13 @@
 db.clientes.aggregate([
   { $match: { sexo: "MASCULINO" } },
 ]);
+
+// Exercício 2: Utilizando o estágio $match , escreva uma agregação para retornar somente os clientes do sexo "FEMININO" e com data de nascimento entre os anos de 1995 e 2005 .
+
+db.clientes.aggregate([
+  { $match: { 
+    $and: [
+      { sexo: "FEMININO" },
+      { dataNascimento: { $gte: ISODate("1995-01-01T00:00:00.0Z"), $lt: ISODate("2006-01-01T00:00:00.000Z")}}
+    ]
+  } }]);
