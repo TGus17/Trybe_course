a3de85f62958f30c415f2e215d9e92784c7b3edd
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-25 20:57:46 -0300

    :construction: Ex. 10

--- Exercicios/MODULO_3_BACKEND/Bloco_25/25_1/exercicio_dia_25_1.js
+++ Exercicios/MODULO_3_BACKEND/Bloco_25/25_1/exercicio_dia_25_1.js
@@ -131,4 +131,30 @@ db.vendas.aggregate([
   { $limit: 10 },
 ]);
 
+// Exercício 10 : Descubra quantos clientes compraram mais de 5 vezes. Retorne um documento que contenha somente o campo clientes com o total de clientes.
 
+db.vendas.aggregate([
+  {
+    $group: {
+      _id: "$clienteId",
+      clientes: {
+        $sum: 1,
+      },
+    }
+  },
+  {
+    $match: {
+      clientes: {
+        $gt: 5,
+      },
+    }
+  },
+  {
+    $count: "clientes",
+  },
+  {
+    $project: {
+      "_id": 0,
+    },
+  },
+]);
