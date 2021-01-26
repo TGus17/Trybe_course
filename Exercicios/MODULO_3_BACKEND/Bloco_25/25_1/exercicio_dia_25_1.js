0b8a0967a62657215aacb975884e8dd83acc6f2e
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-25 21:23:45 -0300

    :construction: Ex. 13

--- Exercicios/MODULO_3_BACKEND/Bloco_25/25_1/exercicio_dia_25_1.js
+++ Exercicios/MODULO_3_BACKEND/Bloco_25/25_1/exercicio_dia_25_1.js
@@ -242,3 +242,50 @@ db.vendas.aggregate([
     }
   }
 ]);
+
+// Exercício 13 : Encontre qual foi o total de vendas e a média de vendas de cada uf no ano de 2019 . Ordene os resultados pelo nome da uf . Retorne os documentos no seguinte formato:
+/*
+{
+  "_id": "MG",
+  "mediaVendas": 9407.129225352113,
+  "totalVendas": 142
+}
+*/
+
+db.vendas.aggregate([
+  {
+    $match: {
+      dataVenda: {
+        $gte: ISODate("2019-01-01"),
+        $lte: ISODate("2019-12-31"),
+      },
+    },
+  },
+  {
+    $lookup: {
+      from: "clientes",
+      localField: "clienteId",
+      foreignField: "clienteId",
+      as: "info",
+    },
+  },
+  {
+    $unwind: "$info",
+  },
+  {
+    $group: {
+      _id: "$info.endereco.uf",
+      totalVendas: {
+        $sum: 1,
+      },
+      mediaVendas: {
+        $avg: "$valorTotal"
+      }
+    },
+  },
+  {
+    $sort: {
+      "_id": 1,
+    },
+  },
+]);
